9fe259fefc2ac950afdddcdee0857e75a0b7d4bb
pablosb01 <pablosb01@icloud.com>
2024-04-13 17:24:16 +0200

    tarde sabado

--- scripts/buttom-movement.js
+++ scripts/buttom-movement.js
@@ -1,5 +1,20 @@
 const menuBtn = document.getElementById('menuBtn');
+const navbar = document.getElementById('navbar');
+
 
 menuBtn.addEventListener('click', function() {
-  this.classList.toggle('active');
+  navbar.classList.toggle('active');
+
+
+  // Alternar el ancho de las líneas del botón
+  const line1 = document.getElementById('line1');
+  const line2 = document.getElementById('line2');
+  const line3 = document.getElementById('line3');
+
+  line1.style.width = line1.style.width === '100%' ? '80%' : '100%';
+  line2.style.width = line2.style.width === '100%' ? '50%' : '100%';
+  line3.style.width = line3.style.width === '100%' ? '70%' : '100%';
+
+  // Alternar la clase de la barra de navegación para cambiar su anchura
+  navbar.classList.toggle('narrow');
 });
